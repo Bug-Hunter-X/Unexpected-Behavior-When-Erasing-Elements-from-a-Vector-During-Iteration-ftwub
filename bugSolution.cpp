4597b448874c07acd2dc16a273aa5194d958be70
BugHunter-X <askzenai@gmail.com>
2025-01-01 20:18:05 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,1 @@
+std::vector<int> vec; for (int i = 0; i < 10; ++i) { vec.push_back(i); } for (auto it = vec.begin(); it != vec.end(); ) { std::cout << *it << " "; if (*it % 2 == 0) { it = vec.erase(it); } else { ++it; } }
